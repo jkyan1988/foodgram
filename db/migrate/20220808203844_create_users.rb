f72eac02917db1df3296e50626248cd3c6900515
James Kyan <james.kyan88@gmail.com>
2022-08-09 10:04:32 -0400

    heroku push

---
+++ db/migrate/20220808203844_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.integer :posts
+      t.string :profile_pic
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+
+puts "Seeding users..."
+
+10.times do 
+    User.create!(username: Faker::Name.first_name, password: "123", profile_pic: "https://i.pravatar.cc/150?img=100", bio: Faker::Restaurant.description)
+end
+
+puts "Seeding posts..."
+
+10.times do
+    Post.create!(post: "https://picsum.photos/800/900", description: Faker::Restaurant.description)
+end
+
+puts "Seeding comments..."
+10.times do
+    Comment.create!(comment: Faker::Restaurant.description, user_id: User.all.ids.sample, post_id: Post.all.ids.sample)
+end
+
+
+puts "Seeding completed successfully"
